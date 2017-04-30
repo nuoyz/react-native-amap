36c1d23e3d2b2de9533cea8dcd54e9455f7bebd9
周帅 <shuai.zhou@kongming-inc.com>
2017-05-01 02:27:54 +0800

    显示定位蓝点

--- android/app/src/main/java/com/testamap/ReactAmapManager.java
+++ android/app/src/main/java/com/testamap/ReactAmapManager.java
@@ -1,6 +1,15 @@
 package com.testamap;
 
+import android.util.Log;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 
@@ -8,10 +17,13 @@ import com.facebook.react.uimanager.ThemedReactContext;
  * Created by ASUS on 2017/4/26.
  */
 
-public class ReactAmapManager extends SimpleViewManager<MapView> {
+public class ReactAmapManager extends SimpleViewManager<MapView> implements LocationSource, AMapLocationListener {
     public static final String REACT_CLASS = "MapView";
    // private Object mCallerContext;
-
+    AMap aMap = null;
+    LocationSource.OnLocationChangedListener mListener;
+    AMapLocationClient mlocationClient;
+    AMapLocationClientOption mLocationOption;
     @Override
     public String getName() {
         return REACT_CLASS;
@@ -21,6 +33,93 @@ public class ReactAmapManager extends SimpleViewManager<MapView> {
     public MapView createViewInstance(ThemedReactContext context) {
         MapView mapView = new MapView(context);//mCallerContext
         mapView.onCreate(null);
+        if (aMap == null) {
+            aMap = mapView.getMap();
+            init();
+        }
         return mapView;
+    }
+    public void init() {
+        aMap.setLocationSource(this);//this 用法
+        // 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        aMap.setMyLocationEnabled(true);
+        // 设置定位的类型为定位模式，有定位、跟随或地图根据面向方向旋转几种
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+        setLocationType();
+    }
+
+    @Override
+    public void activate(LocationSource.OnLocationChangedListener listener) {
+        mListener = listener;
+        if (mlocationClient == null) {
+            //初始化定位
+            mlocationClient = new AMapLocationClient(this);
+            //初始化定位参数
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位回调监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            //获取一次定位结果：
+            //该方法默认为false。
+            mLocationOption.setOnceLocation(true);
+
+            //获取最近3s内精度最高的一次定位结果：
+            //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+            mLocationOption.setOnceLocationLatest(true);
+            mlocationClient.startLocation();//启动定位
+
+        }
+    }
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+    /**
+     * 定位成功后回调函数
+     */
+    //@Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (mListener != null&&amapLocation != null) {
+            if (amapLocation != null
+                    &&amapLocation.getErrorCode() == 0) {
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+                Log.e("AmapErr","成功定位");
+            } else {
+                String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
+                Log.e("AmapErr",errText);
+            }
+        }
+    }
+
+    public void setLocationType() {
+        MyLocationStyle myLocationStyle = new MyLocationStyle();//初始化定位蓝点
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW );//连续定位等等 1.连续 太频繁 2.单次
+        myLocationStyle.interval(1000);//设置连续定位模式下的定位间隔，只在连续定位模式下有效
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的style
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必须设置
+        aMap.setMyLocationEnabled(true);//设置true表示启动显示定位蓝点，false表示隐藏定位蓝点，并不进行定位 默认为false
+
+        //MyLocationStyle myLocationIcon(BitmapDescriptor myLocationIcon);//设置定位蓝点的icon图标方法，需要用到BitmapDescriptor类对象作为
+        //这里的icon 之后可以暴露出一个接口供web使用
+
+        /*//获取一次定位结果：
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true);
+
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);*/
     }
 }
